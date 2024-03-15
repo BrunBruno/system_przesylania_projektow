f4d4d7a5c7850ef3af7f998b100df4c92f272aaa
DESKTOP-D0EL4H2\DST <brunomaslon@gmail.com>
2024-03-15 08:56:19 +0100

    first commit

---
+++ system_przesylania_projektow/system_przesylania_projektow/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace system_przesylania_projektow
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
